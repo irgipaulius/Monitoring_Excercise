206375b915afe69b8e3d54310779abd90a4788be
Paulius <irgipaulius@gmail.com>
2019-10-06 19:03:12 +0200

    updated db

--- monitoring.sql
+++ monitoring.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2019 at 12:10 PM
+-- Generation Time: Oct 06, 2019 at 07:02 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
